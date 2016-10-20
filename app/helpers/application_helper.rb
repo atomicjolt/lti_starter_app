bafa6697349ac57d7acb533630d67d0042768cb4
Justin Ball <justinball@gmail.com>
2016-10-20 16:02:05 -0600

    cleanup helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,15 +10,18 @@ module ApplicationHelper
 
   def jwt_token
     return unless signed_in?
-    AuthToken.issue_token({ user_id: current_user.id, canvas_roles: params["roles"] })
+    AuthToken.issue_token({
+      user_id: current_user.id,
+      lti_roles: params["roles"]
+    })
   end
 
   def course_id
-    return params[:custom_canvas_course_id]
+    params[:custom_canvas_course_id]
   end
 
   def user_id
-    return params[:custom_canvas_user_id]
+    params[:custom_canvas_user_id]
   end
 
 end
