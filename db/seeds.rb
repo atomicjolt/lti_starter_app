f75b92ff73adf5e2f3d30a91973abf8f6c29d612
Justin Ball <justinball@gmail.com>
2016-10-27 11:16:55 -0600

    Updates example seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,13 @@ puts 'CREATED ADMIN USER: ' << admin.email
 # Add an LTI Application
 lti_applications = [{
   name: "LTI Starter App",
-  lti_key: "lti-starter-app",
   description: "LTI Starter App by Atomic Jolt",
-  client_application_name: "app",
+  lti_key: "lti-starter-app",
+  lti_secret: Rails.application.secrets.scorm_lti_key,
   lti_consumer_uri: "https://atomicjolt.instructure.com",
-  canvas_api_permissions: "LIST_ACCOUNTS" # List Canvas API methods the app is allowed to use. A full list of constants can be found in canvas_urls
+  client_application_name: "app",
+  canvas_api_permissions: "LIST_ACCOUNTS", # List Canvas API methods the app is allowed to use. A full list of constants can be found in canvas_urls
+  canvas_token: Rails.application.secrets.canvas_token # This is only required if the app needs API access and doesn't want each user to do the oauth dance
 }]
 
 lti_applications.each do |attrs|
