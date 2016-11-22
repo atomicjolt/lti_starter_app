446f9ad83c484d0dce29042025ba79a6a6102d39
Justin Ball <justinball@gmail.com>
2016-11-21 19:58:33 -0700

    Allow turning of transactional fixtures per spec

---
+++ spec/support/transactions.rb
@@ -0,0 +1,15 @@
+# Tests that require pessimistic db locking require transactional fixtures false.
+# Allow this functionality on a per test basis
+def without_transactional_fixtures(&block)
+  self.use_transactional_fixtures = false
+
+  before(:all) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  yield
+
+  after(:all) do
+    DatabaseCleaner.strategy = :transaction
+  end
+end
