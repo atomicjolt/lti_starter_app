30bc38b8a1d0aa1b7bc319cbbe56f5e82e688f72
Justin Ball <justinball@gmail.com>
2016-12-08 23:01:20 -0700

    Remove feature spec

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'cancancan'
 gem 'rolify'
 gem 'attr_encrypted'
 gem 'jwt', '~> 1.5.0' # json web token
-gem 'lms-api', path: '../lms-api'
+gem 'lms-api'
 
 # Email
 gem 'sendgrid'
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,14 +1,14 @@
-require 'rails_helper'
-Capybara.default_driver = :selenium
-Capybara.default_max_wait_time = 30
-WebMock.allow_net_connect!
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
+# require 'rails_helper'
+# Capybara.default_driver = :selenium
+# Capybara.default_max_wait_time = 30
+# WebMock.allow_net_connect!
+# Capybara.register_driver :selenium do |app|
+#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
+# end
 
-feature 'user opens the home page' do
+# feature 'user opens the home page' do
 
-  scenario 'user views the home page', js: true do
-    visit "/"
-  end
-end
+#   scenario 'user views the home page', js: true do
+#     visit "/"
+#   end
+# end
