37a56c57906165692b51478afab16a962e365486
Justin Ball <justinball@gmail.com>
2017-01-13 15:57:08 -0700

    Fixes spec

--- client/js/components/common/canvas_authentication.spec.jsx
+++ client/js/components/common/canvas_authentication.spec.jsx
@@ -1,12 +1,12 @@
-import React                from 'react';
-import TestUtils            from 'react-addons-test-utils';
-import CanvasAuthentication from './canvas_authentication';
-import Stub                 from '../../../specs_support/stub';
+import React                    from 'react';
+import TestUtils                from 'react-addons-test-utils';
+import { CanvasAuthentication } from './canvas_authentication';
+import Stub                     from '../../../specs_support/stub';
 
 describe('Canvas authentication', () => {
   it('renders a button to authenticate with Canvas', () => {
     const result = TestUtils.renderIntoDocument(<Stub><CanvasAuthentication /></Stub>);
-    const heading = TestUtils.findRenderedDOMComponentWithTag(result, 'h1');
-    expect(heading.textContent).toContain('Page Not Found');
+    const a = TestUtils.findRenderedDOMComponentWithTag(result, 'a');
+    expect(a.textContent).toContain('Authorize Application');
   });
 });
