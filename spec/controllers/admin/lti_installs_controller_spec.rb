72744978305486eb6ee4c42a40cb4b2ea2e22486
Justin Ball <justinball@gmail.com>
2017-01-13 14:13:04 -0700

    Fixes failing spec

--- spec/controllers/admin/lti_installs_controller_spec.rb
+++ spec/controllers/admin/lti_installs_controller_spec.rb
@@ -1,21 +1,14 @@
 require "rails_helper"
 
 RSpec.describe Admin::LtiInstallsController, type: :controller do
-
   before do
     @application = FactoryGirl.create(:lti_application)
   end
 
   context "not logged in" do
-    describe "GET new" do
+    describe "GET index" do
       it "redirects the user to the canvas authentication page" do
-        get :new, {account_ids: [1]}
-        expect(response).to have_http_status(302)
-      end
-    end
-    describe "POST create" do
-      it "redirects the user to the canvas authentication page" do
-        post :create, { lti_install: {account_ids: ["43460000000000017"]}, lti_application_id: @application.id }
+        get :index
         expect(response).to have_http_status(302)
       end
     end
@@ -24,9 +17,9 @@ RSpec.describe Admin::LtiInstallsController, type: :controller do
   context "as user" do
     login_user
 
-    describe "GET new" do
+    describe "GET index" do
       it "redirects the user to the canvas authentication page" do
-        get :new, {account_ids: [1]}
+        get :index
         expect(response).to have_http_status(401)
       end
     end
@@ -36,48 +29,15 @@ RSpec.describe Admin::LtiInstallsController, type: :controller do
   context "as admin" do
     login_admin
 
-    before do
-      authentication = FactoryGirl.create(:authentication, provider: 'canvas')
-      @admin.authentications << authentication
-    end
-
-    describe "GET new" do
+    describe "GET index" do
       context "user has authenticated with canvas" do
-        before do
-          @authentication = FactoryGirl.create(:authentication, user: @user, provider: 'canvas')
-        end
-        it "displays a form for the user to specify the accounts into which to install the LTI tool" do
-          get :new
+        it "displays the admin lti tool" do
+          get :index
           expect(response).to have_http_status(200)
         end
       end
     end
 
-    describe "POST create" do
-      context "user has authenticated with canvas" do
-        before do
-          @authentication = FactoryGirl.create(:authentication, user: @user, provider: 'canvas')
-        end
-        # TODO have to figure out the admin side of things
-        # it "Adds the LTI tool to the specified accounts" do
-        #   post :create, { lti_install: {account_ids: ["43460000000000017"], course_ids: [""]}, lti_application_id: @application.id }
-        #   expect(response).to have_http_status(200)
-        # end
-        # it "Adds the LTI tool to the specified courses" do
-        #   post :create, { lti_install: {account_ids: [""], course_ids: ["43460000000000228"]}, lti_application_id: @application.id }
-        #   expect(response).to have_http_status(200)
-        # end
-      end
-      context "No accounts selected" do
-        before do
-          @authentication = FactoryGirl.create(:authentication, user: @user, provider: 'canvas')
-        end
-        it "Indicates the LTI tool was not installed" do
-          post :create, { lti_install: {account_ids: [], course_ids: []}, lti_application_id: @application.id }
-          expect(response).to have_http_status(200)
-        end
-      end
-    end
   end
 
 end
