744386291b6871445a477d3283e5636a02858802
Benjamin Call <llacneb@gmail.com>
2017-01-17 14:37:09 -0700

    getting real data

--- app/controllers/admin/app_instances_controller.rb
+++ app/controllers/admin/app_instances_controller.rb
@@ -1,15 +1,15 @@
 class Admin::AppInstancesController < ApplicationController
 
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :only_admins!
 
   def index
-    applications = Application.all
+    applications = ApplicationInstance.all
   end
 
   private
 
-    def only_admins!
-      user_not_authorized unless current_user.admin?
-    end
+  def only_admins!
+    user_not_authorized unless current_user.admin?
+  end
 end
