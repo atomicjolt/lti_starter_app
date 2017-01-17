acd0735c4cdb3782cdc9666eb59194cb7519291a
dspencer001 <dspencer577@gmail.com>
2017-01-17 11:20:50 -0700

    Add routes for admin tool

--- client/js/admin_routes.jsx
+++ client/js/admin_routes.jsx
@@ -4,13 +4,13 @@ import { Router, Route, IndexRoute }  from 'react-router';
 
 import appHistory                     from './history';
 import Index                          from './components/layout/index';
-import Home                          from './components/home';
+import Admin                          from './components/admin/lti_applications/index';
 import NotFound                       from './components/common/not_found';
 
 export default (
   <Router history={appHistory}>
     <Route path="/" component={Index}>
-      <IndexRoute component={Home} />
+      <IndexRoute component={Admin} />
     </Route>
     <Route path="*" component={NotFound} />
   </Router>
