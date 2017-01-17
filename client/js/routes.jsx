a50839de795ca4d376397fc9f8d562cb9d3f5c68
dittonjs <jditton.atomic@gmail.com>
2017-01-17 11:26:18 -0700

    update routes

--- client/js/routes.jsx
+++ client/js/routes.jsx
@@ -6,7 +6,6 @@ import appHistory                     from './history';
 import Index                          from './components/layout/index';
 import Home                          from './components/home';
 import NotFound                       from './components/common/not_found';
-import Instances                      from './components/admin/instances/_instances';
 
 export default (
   <Router history={appHistory}>
