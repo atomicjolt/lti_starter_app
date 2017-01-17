7d582ba4ce3f819c3bab63572a5f22fd149f46fc
bensmith <benhsmith13@gmail.com>
2017-01-17 14:18:21 -0700

    hooks the action up, kinda

--- client/js/reducers/instances.js
+++ client/js/reducers/instances.js
@@ -3,6 +3,10 @@ const initialState = {};
 export default function instances(state = initialState, action) {
   switch (action.type) {
 
+    case 'GET_INSTANCES_DONE':
+      debugger;
+      return state;
+
     default:
       return state;
   }
