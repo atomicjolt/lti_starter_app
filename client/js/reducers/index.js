522ab99a0020ed68bb38dc77a22d5d761fc831c8
bensmith <benhsmith13@gmail.com>
2017-01-17 11:23:53 -0700

    forgot the index route

--- client/js/reducers/index.js
+++ client/js/reducers/index.js
@@ -3,12 +3,14 @@ import settings            from './settings';
 import application         from './application';
 import jwt                 from './jwt';
 import ltiApplications     from './lti_applications';
+import instances           from './instances';
 
 const rootReducer = combineReducers({
   settings,
   jwt,
   application,
   ltiApplications,
+  instances,
 });
 
 export default rootReducer;
--- client/js/reducers/instances.js
+++ client/js/reducers/instances.js
@@ -1,6 +1,6 @@
 const initialState = {};
 
-export default function instances(state = initialState, action){
+export default function instances(state = initialState, action) {
   switch (action.type) {
 
     default:
