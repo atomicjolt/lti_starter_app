9241fd5d6af65e60fa71eb5a2e3de70cdab31c90
bensmith <benhsmith13@gmail.com>
2017-01-18 10:52:47 -0700

    A settings dropdown

--- client/js/components/admin/instances/instance_header.jsx
+++ client/js/components/admin/instances/instance_header.jsx
@@ -1,35 +1,49 @@
 import React    from 'react';
+import DropDown from './settings_drop_down';
 
-export default function instanceHeader(props) {
-
-  const styles = {
-    buttonIcon: {
-      border          : 'none',
-      backgroundColor : 'transparent',
-      color           : 'grey',
-      fontSize        : '1em',
-      cursor          : 'pointer',
-    }
+export default class InstanceHeader extends React.Component {
+  static propTypes = {
+    newInstance  : React.PropTypes.func.isRequired,
   };
 
-  return (
-    <div className="c-info">
-      <div className="c-title">
-        <h1>Instances</h1>
-        <h3>Attendance
-          <button style={styles.buttonIcon} onClick={props.openSettings}>
-            <i className="i-settings" />
-          </button>
-        </h3>
-      </div>
-      <button className="c-btn c-btn--yellow" onClick={props.newInstance}>
-        New Instance
-      </button>
-    </div>
-  );
-}
+  constructor() {
+    super();
+    this.state = { settingsOpen: false }
+  }
 
-instanceHeader.propTypes = {
-  openSettings : React.PropTypes.func.isRequired,
-  newInstance  : React.PropTypes.func.isRequired,
-};
+  getStyles() {
+    return {
+      buttonIcon: {
+        border          : 'none',
+        backgroundColor : 'transparent',
+        color           : 'grey',
+        fontSize        : '1em',
+        cursor          : 'pointer',
+      },
+    };
+  }
+
+  render() {
+    const styles = this.getStyles();
+
+    return (
+      <div className="c-info">
+        <div className="c-title">
+          <h1>Instances</h1>
+          <h3>Attendance
+            <button
+              style={styles.buttonIcon}
+              onClick={() => this.setState({ settingsOpen: !this.state.settingsOpen })}
+            >
+              <i className="i-settings" />
+            </button>
+          </h3>
+          { this.state.settingsOpen ? <DropDown /> : null }
+        </div>
+        <button className="c-btn c-btn--yellow" onClick={this.props.newInstance}>
+          New Instance
+        </button>
+      </div>
+    );
+  }
+}
