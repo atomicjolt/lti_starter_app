7551d2426ca565e2be02733a069f200fd31ae74b
dspencer001 <dspencer577@gmail.com>
2017-01-18 12:53:40 -0700

    Add a class to the index component div

--- client/js/components/layout/index.jsx
+++ client/js/components/layout/index.jsx
@@ -13,7 +13,7 @@ export default class Index extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="app-index">
         {this.props.children}
       </div>
     );
