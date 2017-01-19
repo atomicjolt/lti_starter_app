bf4b7a0bcabf3da17786f6921f3d038ce67e5b26
Justin Ball <justinball@gmail.com>
2017-01-18 18:26:07 -0700

    Fix auth component

--- client/js/components/home.jsx
+++ client/js/components/home.jsx
@@ -2,12 +2,15 @@ import React       from 'react';
 import { connect } from 'react-redux';
 import assets      from '../libs/assets';
 
+import Auth    from './common/canvas_authentication'
+
 export default class Home extends React.Component {
 
   render(){
     const img = assets('./images/atomicjolt.jpg');
     return (
       <div>
+        <Auth />
         <img src={img} alt="Atomic Jolt Logo" />
       </div>
     );
