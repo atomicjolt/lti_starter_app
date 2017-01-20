4e0c4823d5308c6bcfdfe2494622584cd748b150
Benjamin Call <llacneb@gmail.com>
2017-01-20 11:54:37 -0700

    better styles

--- client/js/components/admin/common/heading.jsx
+++ client/js/components/admin/common/heading.jsx
@@ -3,33 +3,33 @@ import assets       from '../../../libs/assets';
 
 const Heading = () => {
   const img = assets('./images/atomicjolt.svg');
+  const styles = {
+    bar: {
+      background: 'black',
+      height: '50px',
+      borderBottom: 'solid #EFC210 1px',
+      display: 'block',
+    },
+    h1: {
+      textAlign: 'center',
+      color: '#EFC210',
+      paddingTop: '5px',
+      paddingLeft: '20px',
+    },
+    color: {
+      float: 'right',
+      paddingTop: '10px',
+      paddingRight: '20px',
+      color: 'lightgrey',
+      fontFamily: 'Roboto, sans-serif',
+    }
+  };
+
   return (
-    <div
-      style={{
-        background: 'black',
-        height: '50px',
-        borderBottom: 'solid #EFC210 1px',
-        display: 'block',
-      }}
-    >
-      <h1
-        style={{
-          textAlign: 'center',
-          color: '#EFC210',
-          paddingTop: '5px',
-          paddingLeft: '20px',
-        }}
-      >
+    <div style={styles.bar}>
+      <h1 style={styles.h1}>
         <img src={img} alt="AtomicJolt Logo" height="40px" />
-        <div
-          style={{
-            float: 'right',
-            paddingTop: '10px',
-            paddingRight: '20px',
-            color: 'lightgrey',
-            fontFamily: 'Roboto, sans-serif',
-          }}
-        >
+        <div style={styles.color}>
           pronto
         </div>
       </h1>
