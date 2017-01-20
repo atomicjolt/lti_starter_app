02e097929a3b83464fd015e411bce968bda6ec49
Benjamin Call <llacneb@gmail.com>
2017-01-20 11:40:49 -0700

    statless now

--- client/js/components/admin/common/heading.jsx
+++ client/js/components/admin/common/heading.jsx
@@ -1,42 +1,40 @@
 import React        from 'react';
 import assets       from '../../../libs/assets';
 
-export class Heading extends React.Component {
-  render() {
-    const img = assets('./images/atomicjolt.svg');
-    return (
-      <div
+const Heading = () => {
+  const img = assets('./images/atomicjolt.svg');
+  return (
+    <div
+      style={{
+        background: 'black',
+        height: '50px',
+        borderBottom: 'solid #EFC210 1px',
+        display: 'block',
+      }}
+    >
+      <h1
         style={{
-          background: 'black',
-          height: '50px',
-          borderBottom: 'solid #EFC210 1px',
-          display: 'block',
+          textAlign: 'center',
+          color: '#EFC210',
+          paddingTop: '5px',
+          paddingLeft: '20px',
         }}
       >
-        <h1
+        <img src={img} alt="AtomicJolt Logo" height="40px" />
+        <div
           style={{
-            textAlign: 'center',
-            color: '#EFC210',
-            paddingTop: '5px',
-            paddingLeft: '20px',
+            float: 'right',
+            paddingTop: '10px',
+            paddingRight: '20px',
+            color: 'lightgrey',
+            fontFamily: 'Roboto, sans-serif',
           }}
         >
-          <img src={img} alt="AtomicJolt Logo" height="40px" />
-          <div
-            style={{
-              float: 'right',
-              paddingTop: '10px',
-              paddingRight: '20px',
-              color: 'lightgrey',
-              fontFamily: 'Roboto, sans-serif',
-            }}
-          >
-            pronto
-          </div>
-        </h1>
-      </div>
-    );
-  }
-}
+          pronto
+        </div>
+      </h1>
+    </div>
+  );
+};
 
-export default (Heading);
+export default Heading;
