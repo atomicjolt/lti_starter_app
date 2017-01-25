ea6269a92cbdeceade6a2490428a55d10d344d0a
Justin Ball <justinball@gmail.com>
2017-01-24 19:09:12 -0700

    Fixes application instances

--- app/controllers/api/application_instances_controller.rb
+++ app/controllers/api/application_instances_controller.rb
@@ -1,16 +1,25 @@
 class Api::ApplicationInstancesController < Api::ApiApplicationController
 
   load_and_authorize_resource :application
-  load_and_authorize_resource :application_instance, through: :application, param_method: :param_sanitizer
+  load_and_authorize_resource :application_instance, through: :application
 
   def index
-    @application_instances = @application_instances.include(:site)
+    @application_instances = @application_instances.includes(:site)
     render json: @application_instances
+  end
+
+  def create
+    render json: @application_instance
+  end
+
+  def destroy
+    @application_instance.destroy
+    render json: { head: :ok }
   end
 
   private
 
-  def param_sanitizer
+  def application_instance_params
     params.require(:application_instance).permit(:name)
   end
 
