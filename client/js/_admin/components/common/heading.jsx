2dc54cb837af041ed7a29241a26ccd1401cf5628
bensmith <benhsmith13@gmail.com>
2017-01-26 18:11:28 -0700

    adds logout

--- client/js/_admin/components/common/heading.jsx
+++ client/js/_admin/components/common/heading.jsx
@@ -1,28 +1,60 @@
-import React        from 'react';
-import assets       from '../../../libs/assets';
+import React            from 'react';
+import assets           from '../../../libs/assets';
+import UserDropdown     from '../common/user_dropdown';
 
-export default function heading(props) {
-  const img = assets('./images/atomicjolt.svg');
+export default class heading extends React.Component {
+  static propTypes = {
+    back: React.PropTypes.func,
+    userName: React.PropTypes.string,
+  };
 
-  return (
-    <header className="c-head">
-      <div className="c-back">
-        {
-          props.back ? <button className="c-btn c-btn--back" onClick={props.back}>
-            <i className="i-back" />
-            Back
-          </button> : null
-        }
-      </div>
-      <img className="c-logo" src={img} alt="Atomic Jolt Logo" />
-      <ul className="c-user">
-        <li>
-          <div className="c-username">{props.userName}<i className="i-dropdown" /></div>
-        </li>
-      </ul>
-    </header>
-  );
-};
+  constructor() {
+    super();
+    this.state = { showDropDown: false };
+  }
+
+  getStyles() {
+    return {
+      userName: {
+        backgroundColor: 'transparent',
+        border: 'none',
+        cursor: 'pointer'
+      }
+    };
+  }
+
+  render() {
+    const img = assets('./images/atomicjolt.svg');
+    const styles = this.getStyles();
+
+    return (
+      <header className="c-head">
+        <div className="c-back">
+          {
+            this.props.back ? <button className="c-btn c-btn--back" onClick={this.props.back}>
+              <i className="i-back" />
+              Back
+            </button> : null
+          }
+        </div>
+        <img className="c-logo" src={img} alt="Atomic Jolt Logo" />
+        <ul className="c-user">
+          <li>
+            <button
+              className="c-username"
+              style={styles.userName}
+              onClick={() => this.setState({ showDropDown: !this.state.showDropDown })}
+            >
+              {this.props.userName}
+              <i className="i-dropdown" />
+            </button>
+          </li>
+        </ul>
+        {this.state.showDropDown ? <UserDropdown /> : null }
+      </header>
+    );
+  }
+}
 
 heading.propTypes = {
   back: React.PropTypes.func,
--- client/js/_admin/components/common/user_dropdown.jsx
+++ client/js/_admin/components/common/user_dropdown.jsx
@@ -1,5 +1,5 @@
 import React      from 'react';
-import Item       from '../common/drop_down_item';
+import Item       from './drop_down_item';
 
 export default function settingDropDown(props) {
   const styles = {
@@ -8,6 +8,8 @@ export default function settingDropDown(props) {
       backgroundColor : 'white',
       fontSize        : '2em',
       position        : 'absolute',
+      right           : '10px',
+      top             : '75%',
       border          : '1px solid black',
       borderRadius    : '5px',
       padding         : '5px',
@@ -16,13 +18,8 @@ export default function settingDropDown(props) {
 
   return (
     <ul style={styles.settingsDropDown}>
-      <Item>Settings</Item>
-      <Item>Other Settings</Item>
-      <Item>More Settings</Item>
+      <Item><div dangerouslySetInnerHTML={{ __html: window.SIGN_OUT }} /></Item>
+      <Item>Logout and Delete Authentications</Item>
     </ul>
   );
 }
-
-settingDropDown.propTypes = {
-
-};
