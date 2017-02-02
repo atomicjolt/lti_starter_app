9be0af4a678fb206b7674c6623a8d4a5cccf4916
dspencer001 <dspencer577@gmail.com>
2017-02-02 12:10:17 -0700

    Always expect sub accounts, update with how deep was clicked

--- client/js/_admin/components/lti_installs/sub_accounts.jsx
+++ client/js/_admin/components/lti_installs/sub_accounts.jsx
@@ -3,52 +3,50 @@ import _                   from 'lodash';
 import { getSubAccountsOfAccount } from '../../../libs/canvas/constants/accounts';
 
 export default class SubAccounts extends React.Component {
-
-  getSubAccounts(account) {
-    const { activeAccount } = this.props;
-    this.props.canvasRequest(getSubAccountsOfAccount, { account_id: account.id }, {}, account);
-    this.props.setAccount(activeAccount && activeAccount.id === account.id ? null : account);
+  isActive(account) {
+    const { activeAccounts } = this.props;
+    return !!_.find(
+      activeAccounts,
+      activeAccount => activeAccount.id === account.id
+    );
   }
 
-  shouldShowAccounts(account) {
-    const { activeAccount } = this.props;
-    return account.subAccounts && activeAccount
-      && (activeAccount.id === account.id || activeAccount.parent_account_id === account.id);
-  }
+  accounts(accounts, depth) {
+    return _.map(accounts, (account) => {
+      const isActive = this.isActive(account);
 
-  accounts(accounts) {
-    const { activeAccount } = this.props;
-
-    return _.map(accounts, account => (
-      <li
-        key={`account_${account.id}`}
-        className={activeAccount && activeAccount.id === account.id ? 'c-filter__item is-active' : 'c-filter__item'}
-      >
-        <span>
-          <i className="i-dropdown" />
-          {account.name}
-        </span>
-        {
-          this.shouldShowAccounts(account) ? <ul className="c-filter__dropdown">
-            {this.accounts(account.subAccounts)}
-          </ul> : null
-        }
-      </li>
-    ));
+      return (
+        <li
+          key={`account_${account.id}`}
+          className={isActive ? 'c-filter__item is-active' : 'c-filter__item'}
+        >
+          <button onClick={() => this.props.setAccountActive(account, depth)}>
+            { _.size(account.sub_accounts) > 0 ? <i className="i-dropdown" /> : null }
+            {account.name}
+          </button>
+          {
+            isActive ? <ul className="c-filter__dropdown">
+              {this.accounts(account.sub_accounts, depth + 1)}
+            </ul> : null
+          }
+        </li>
+      );
+    });
   }
 
   render() {
+    // passing zero to keep track of how deep we are in the tree
     return (
       <ul className="c-filter__dropdown">
-        {this.accounts(this.props.accounts)}
+        {this.accounts(this.props.accounts, 0)}
       </ul>
     );
   }
 }
 
 SubAccounts.propTypes = {
-  accounts: React.PropTypes.shape({}),
+  accounts: React.PropTypes.arrayOf(React.PropTypes.shape({})),
   canvasRequest: React.PropTypes.func.isRequired,
-  setAccount: React.PropTypes.func.isRequired,
-  activeAccount: React.PropTypes.shape({}),
+  setAccountActive: React.PropTypes.func.isRequired,
+  activeAccounts: React.PropTypes.arrayOf(React.PropTypes.number),
 };
