c5205888fbade6b77b363948a9e3dc884415e5c3
dspencer001 <dspencer577@gmail.com>
2017-02-02 12:08:32 -0700

    Recursively get sub accounts, display a loading modal

--- client/js/_admin/components/lti_installs/index.jsx
+++ client/js/_admin/components/lti_installs/index.jsx
@@ -1,10 +1,14 @@
 import React                 from 'react';
 import { connect }           from 'react-redux';
 import _                     from 'lodash';
+import ReactModal            from 'react-modal';
 import appHistory            from '../../history';
 import { getapplications }   from '../../actions/applications';
-import { getCanvasAccounts } from '../../actions/accounts';
-import { listActiveCoursesInAccount } from '../../../libs/canvas/constants/accounts';
+import * as AccountActions   from '../../actions/accounts';
+import {
+  listActiveCoursesInAccount,
+  getSubAccountsOfAccount
+ } from '../../../libs/canvas/constants/accounts';
 import { listExternalToolsCourses } from '../../../libs/canvas/constants/external_tools';
 import Heading               from '../common/heading';
 import Sidebar               from './sidebar';
@@ -14,17 +18,37 @@ import canvasRequest       from '../../../libs/canvas/action';
 function select(state) {
   return {
     applications: state.applications,
-    accounts: state.accounts,
-    courses: state.courses,
+    accounts: state.accounts.accounts,
+    loading_accounts: state.accounts.loading,
+    courses: _.sortBy(state.courses, course => course.name),
     userName: state.settings.display_name,
   };
+}
+
+function recursiveGetAccounts(account, canvasReq) {
+  if (account.sub_accounts && account.sub_accounts.length === 0) {
+    return;
+  }
+
+  if (account.sub_accounts === undefined) {
+    canvasReq(
+      getSubAccountsOfAccount,
+      { account_id: account.id },
+      null,
+      account
+    );
+  } else {
+    _.each(account.sub_accounts, (subAccount) => {
+      recursiveGetAccounts(subAccount, canvasReq);
+    });
+  }
 }
 
 export class Home extends React.Component {
   static propTypes = {
     accounts: React.PropTypes.shape({}).isRequired,
     applications: React.PropTypes.shape({}).isRequired,
-    courses: React.PropTypes.shape({}).isRequired,
+    courses: React.PropTypes.arrayOf(React.PropTypes.shape({})).isRequired,
     userName: React.PropTypes.string.isRequired,
     getCanvasAccounts: React.PropTypes.func.isRequired,
     canvasRequest: React.PropTypes.func.isRequired,
@@ -33,7 +57,7 @@ export class Home extends React.Component {
   constructor() {
     super();
     this.state = {
-      account: null,
+      activeAccounts: [],
     };
   }
 
@@ -44,9 +68,19 @@ export class Home extends React.Component {
 
   componentDidUpdate(prevProps) {
     // TODO: this is making a mess
+    if (_.isEmpty(prevProps.accounts) && !_.isEmpty(this.props.accounts)) {
+      _.each(
+        this.props.accounts,
+        account => recursiveGetAccounts(account, this.props.canvasRequest)
+      );
+    }
+
     if (_.isEmpty(this.props.courses) && !_.isEmpty(this.props.accounts)) {
-      const accountId = this.props.accounts[0].id;
-      this.props.canvasRequest(listActiveCoursesInAccount, { account_id: accountId, per_page: 100 });
+      const accountId = this.props.accounts[1].id;
+      this.props.canvasRequest(
+        listActiveCoursesInAccount,
+        { account_id: accountId, per_page: 100 }
+      );
     }
 
     if (_.isEmpty(prevProps.courses) && !_.isEmpty(this.props.courses)) {
@@ -55,13 +89,47 @@ export class Home extends React.Component {
   }
 
   getExternalTools() {
-    _.forEach(this.props.courses, course => (
-    this.props.canvasRequest(listExternalToolsCourses, { course_id: course.id })
-    ));
+    // _.forEach(this.props.courses, course => (
+    // this.props.canvasRequest(listExternalToolsCourses, { course_id: course.id })
+    // ));
+  }
+
+  getSubAccountIds(account) {
+    let ids = [account.id]
+    ids = ids.concat(_.map(account.sub_accounts, (subAccount) => {
+      return _.concat(this.getSubAccountIds(subAccount), subAccount.id);
+    }));
+    return _.flatten(ids);
+  }
+
+  setAccountActive(account, depth) {
+    const activeIndex = _.findIndex(
+      this.state.activeAccounts,
+      activeAccount => (activeAccount.id === account.id)
+    );
+    const activeAccounts = _.slice(this.state.activeAccounts, 0, depth);
+
+    if (activeIndex >= 0) {
+      this.setState({ activeAccounts });
+    } else {
+      this.setState({ activeAccounts: activeAccounts.concat(account) });
+    }
   }
 
   render() {
     const applicationInstanceId = parseInt(this.props.params.applicationInstanceId);
+    let accountCourses = this.props.courses;
+    const lastActiveAccount = _.last(this.state.activeAccounts);
+
+    if (lastActiveAccount) {
+      const filterAccountIds = this.getSubAccountIds(lastActiveAccount);
+
+      accountCourses = _.filter(
+        this.props.courses,
+        course => _.includes(filterAccountIds, course.account_id)
+      );
+    }
+
     return (
       <div style={{ height: '100%' }}>
         <Heading back={() => appHistory.goBack()} />
@@ -70,14 +138,23 @@ export class Home extends React.Component {
             accounts={this.props.accounts}
             application={this.props.applications[applicationInstanceId]}
             canvasRequest={this.props.canvasRequest}
-            setAccount={account => this.setState({ account })}
-            activeAccount={this.state.account}
+            setAccountActive={(account, depth) => this.setAccountActive(account, depth)}
+            activeAccounts={this.state.activeAccounts}
           />
-          <InstallPane courses={this.props.courses} account={this.state.account} />
+          <InstallPane courses={accountCourses} account={this.state.account} />
         </div>
+        <ReactModal
+          isOpen={this.props.loading_accounts}
+          contentLabel="Modal"
+          overlayClassName="c-modal__background"
+          className="c-modal c-modal--newsite is-open c-modal--error loading"
+        >
+          <div className="c-loading-icon" />
+          &nbsp;&nbsp;&nbsp;Loading...
+        </ReactModal>
       </div>
     );
   }
 }
 
-export default connect(select, { getapplications, getCanvasAccounts, canvasRequest })(Home);
+export default connect(select, { getapplications, canvasRequest, ...AccountActions })(Home);
