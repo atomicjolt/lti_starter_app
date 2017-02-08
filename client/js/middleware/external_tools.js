b3478cacc8abc139a29c6a3ae2ac9f9eddc3e5c6
James Carbine <jamescarbine@gmail.com>
2017-02-08 12:12:38 -0700

    more lint updates ignore unused var as that file is deleted in the admin branch

--- client/js/middleware/external_tools.js
+++ client/js/middleware/external_tools.js
@@ -1,7 +1,7 @@
 // import api      from '../libs/api';
 // import { DONE } from '../constants/wrapper';
 
-const ExternalTools = store => next => (action) => {
+const ExternalTools = store => next => (action) => { // eslint-disable-line no-unused-vars
   // call the next middleWare
   next(action);
 };
--- client/js/reducers/lti_applications.js
+++ client/js/reducers/lti_applications.js
@@ -7,15 +7,17 @@ const initialState = {};
 export default (state = initialState, action) => {
   switch (action.type) {
 
-    case LtiApplicationConstants.GET_LTI_APPLICATIONS_DONE:
+    case LtiApplicationConstants.GET_LTI_APPLICATIONS_DONE: {
       const newState = _.cloneDeep(state);
       _.forEach(action.payload.lti_applications, (app) => {
         newState[app.id] = app;
       });
       return newState;
+    }
 
-    default:
+    default: {
       return state;
+    }
 
   }
 };
