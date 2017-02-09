0be74bde77ab7a876cb50a283aca056e8855707c
James Carbine <jamescarbine@gmail.com>
2017-02-09 09:16:40 -0700

    Single line the validations for application models

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,8 +1,7 @@
 class Application < ActiveRecord::Base
 
   has_many :application_instances
-  validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 
   enum kind: [:lti, :admin]
 end
