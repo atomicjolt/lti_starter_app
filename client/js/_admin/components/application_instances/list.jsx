04aa611583f5a8be72bf8699ffd0a297d7957c57
dspencer001 <dspencer577@gmail.com>
2017-02-09 12:31:43 -0700

    Finally fix hound?

--- client/js/_admin/components/application_instances/list.jsx
+++ client/js/_admin/components/application_instances/list.jsx
@@ -2,37 +2,35 @@ import React    from 'react';
 import _        from 'lodash';
 import ListRow from './list_row';
 
-export default class List extends React.Component {
-  static propTypes = {
-    applicationInstances: React.PropTypes.arrayOf(React.PropTypes.shape({})).isRequired,
-    settings: React.PropTypes.shape({}).isRequired,
-    deleteApplicationInstance: React.PropTypes.func.isRequired,
-  };
-
-  render() {
-    return (
-      <table className="c-table c-table--instances">
-        <thead>
-          <tr>
-            <th><span>INSTANCE</span></th>
-            <th><span>LTI KEY</span></th>
-            <th><span>DOMAIN</span></th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {
-            _.map(this.props.applicationInstances, (instance, key) => (
-              <ListRow
-                key={`instance_${key}`}
-                {...instance}
-                settings={this.props.settings}
-                delete={this.props.deleteApplicationInstance}
-              />
-            ))
-          }
-        </tbody>
-      </table>
-    );
-  }
+export default function List(props) {
+  return (
+    <table className="c-table c-table--instances">
+      <thead>
+        <tr>
+          <th><span>INSTANCE</span></th>
+          <th><span>LTI KEY</span></th>
+          <th><span>DOMAIN</span></th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {
+          _.map(props.applicationInstances, (instance, key) => (
+            <ListRow
+              key={`instance_${key}`}
+              {...instance}
+              settings={props.settings}
+              delete={props.deleteApplicationInstance}
+            />
+          ))
+        }
+      </tbody>
+    </table>
+  );
 }
+
+List.propTypes = {
+  applicationInstances: React.PropTypes.arrayOf(React.PropTypes.shape({})).isRequired,
+  // settings: React.PropTypes.shape({}).isRequired,
+  deleteApplicationInstance: React.PropTypes.func.isRequired,
+};
