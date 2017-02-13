b190004e2ef7842c33218289ba08428983b1bcc6
Justin Ball <justinball@gmail.com>
2017-02-13 16:26:19 -0700

    Change to pure function

--- client/js/_admin/components/applications/index.jsx
+++ client/js/_admin/components/applications/index.jsx
@@ -12,46 +12,44 @@ function select(state) {
   };
 }
 
-export class Index extends React.Component {
+export function Index(props) {
 
-  static propTypes = {
-    saveApplication: React.PropTypes.func.isRequired,
-    applications: React.PropTypes.shape({}).isRequired,
-  };
+  const applicationRows = _.map(props.applications, (application, index) => (
+    <ApplicationRow
+      key={index}
+      application={application}
+      saveApplication={props.saveApplication}
+    />
+  ));
 
-  render() {
-    const applicationRows = _.map(this.props.applications, (application, index) => (
-      <ApplicationRow
-        key={index}
-        application={application}
-        saveApplication={this.props.saveApplication}
-      />
-    ));
-
-    return (
-      <div>
-        <Heading />
-        <div className="o-contain o-contain--full">
-          <div className="c-info">
-            <div className="c-title">
-              <h1>LTI Applications</h1>
-            </div>
+  return (
+    <div>
+      <Heading />
+      <div className="o-contain o-contain--full">
+        <div className="c-info">
+          <div className="c-title">
+            <h1>LTI Applications</h1>
           </div>
-          <table className="c-table c-table--lti">
-            <thead>
-              <tr>
-                <th><span>NAME</span></th>
-                <th><span>INSTANCES</span></th>
-              </tr>
-            </thead>
-            <tbody>
-              { applicationRows }
-            </tbody>
-          </table>
         </div>
+        <table className="c-table c-table--lti">
+          <thead>
+            <tr>
+              <th><span>NAME</span></th>
+              <th><span>INSTANCES</span></th>
+            </tr>
+          </thead>
+          <tbody>
+            { applicationRows }
+          </tbody>
+        </table>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+Index.propTypes = {
+  saveApplication: React.PropTypes.func.isRequired,
+  applications: React.PropTypes.shape({}).isRequired,
+};
 
 export default connect(select, { saveApplication })(Index);
