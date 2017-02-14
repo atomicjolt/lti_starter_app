8f7671d97f9a5756a9581de940ba5087894fa66e
Justin Ball <justinball@gmail.com>
2017-02-13 18:09:34 -0700

    Only display LTI applications in the admin UI.

--- app/controllers/api/applications_controller.rb
+++ app/controllers/api/applications_controller.rb
@@ -3,7 +3,7 @@ class Api::ApplicationsController < Api::ApiApplicationController
   load_and_authorize_resource :application
 
   def index
-    render status: 200, json: @applications
+    render status: 200, json: @applications.lti
   end
 
 end
