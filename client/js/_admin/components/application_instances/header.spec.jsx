ed341ec3f8dbccfaaa741f98ade3733a6034b72e
Justin Ball <justinball@gmail.com>
2017-02-13 20:37:48 -0700

    Add test for header

---
+++ client/js/_admin/components/application_instances/header.spec.jsx
@@ -0,0 +1,34 @@
+import React     from 'react';
+import TestUtils from 'react-addons-test-utils';
+import Stub      from '../../../../specs_support/stub';
+import Header    from './header';
+
+describe('header', () => {
+
+  let result;
+  const props = {
+    application: {
+      name: 'test application',
+    },
+    newApplicationInstance: () => {},
+  };
+
+  beforeEach(() => {
+    result = TestUtils.renderIntoDocument(
+      <Stub>
+        <Header {...props} />
+      </Stub>
+    );
+  });
+
+  it('renders the header with the app name', () => {
+    const h1 =  TestUtils.findRenderedDOMComponentWithTag(result, 'h1');
+    expect(h1.textContent).toContain(props.application.name);
+  });
+
+  it('renders new application instance button', () => {
+    const button =  TestUtils.findRenderedDOMComponentWithTag(result, 'button');
+    expect(button.textContent).toBe('New Application Instance');
+  });
+
+});
