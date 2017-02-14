f1644bd8e3e198da78541bb193ab0091335aca14
Justin Ball <justinball@gmail.com>
2017-02-13 21:18:50 -0700

    Adds specs

---
+++ client/js/_admin/components/application_instances/index.spec.jsx
@@ -0,0 +1,41 @@
+import React        from 'react';
+import TestUtils    from 'react-addons-test-utils';
+import { Provider } from 'react-redux';
+import Helper       from '../../../../specs_support/helper';
+import { Index }    from './index';
+
+describe('application instances index', () => {
+
+  let result;
+  let applicationInstances = false;
+
+  const props = {
+    applicationInstances: [],
+    getApplicationInstances: () => { applicationInstances = true; },
+    createApplicationInstance: () => {},
+    deleteApplicationInstance: () => {},
+    sites: {},
+    applications: {},
+    params: {
+      applicationId: 'id',
+    },
+    settings: {},
+  };
+
+  beforeEach(() => {
+    result = TestUtils.renderIntoDocument(
+      <Provider store={Helper.makeStore()}>
+        <Index {...props} />
+      </Provider>
+    );
+  });
+
+  it('renders', () => {
+    expect(result).toBeDefined();
+  });
+
+  it('Loads application instances', () => {
+    expect(applicationInstances).toBe(true);
+  });
+
+});
