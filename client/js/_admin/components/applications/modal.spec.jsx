72962a76a5858e2ceae47f7ce8d61e29eb1ddd2f
Francisco <fjab92@gmail.com>
2017-02-16 19:55:49 -0700

    Creating specs for modal and app row

---
+++ client/js/_admin/components/applications/modal.spec.jsx
@@ -0,0 +1,44 @@
+import React         from 'react';
+import TestUtils     from 'react-addons-test-utils';
+import { Provider }  from 'react-redux';
+import Helper        from '../../../../specs_support/helper';
+import Modal         from './modal';
+
+
+describe('applications modal', () => {
+    let result;
+    let props;
+    let isShown;
+    beforeEach(() => {
+      isShown = true;
+      props = {
+        application: {
+          name: "SPEC_NAME",
+          description: "SPEC_STRING"
+        },
+        isOpen: true,
+        closeModal: () => { isShown = false; },
+        save: () => {}
+      };
+
+      result = TestUtils.renderIntoDocument(
+        <Provider store={Helper.makeStore()} >
+          <Modal {...props} />
+        </Provider>
+      );
+    });
+
+    it('modal is shown', () => {
+      let modal = TestUtils.findRenderedComponentWithType(result, Modal);
+      // debugger;
+      expect(modal).toBeDefined();
+      expect(modal.props.isOpen).toBeTruthy();
+    });
+
+    it('modal is hidden', () => {
+      let modal = TestUtils.findRenderedComponentWithType(result, Modal);
+      expect(modal).toBeDefined();
+      modal.props.closeModal();
+      expect(isShown).toBeFalsy();
+    });
+});
