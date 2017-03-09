44dda8bdd9ce7c2d794598715ee8dea7fd389f7c
James Carbine <jamescarbine@gmail.com>
2017-03-09 12:42:29 -0700

    More specs

---
+++ client/js/_admin/components/common/textarea.spec.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import Stub from '../../../../specs_support/stub';
+import Textarea from './textarea';
+
+describe('textarea', () => {
+
+  let result;
+  const props = {
+    inputProps: {
+      id: 'IM AN ID',
+      value: 'IM A VALUE',
+      name: 'the name',
+      onChange: () => {},
+    },
+    className: 'imaclass',
+    labelText: 'IMA LABEL',
+  };
+
+  describe('textarea', () => {
+
+    beforeEach(() => {
+      result = TestUtils.renderIntoDocument(
+        <Stub>
+          <Textarea {...props} />
+        </Stub>
+      );
+    });
+
+    it('renders the textarea with the correct attributes', () => {
+      const textarea = TestUtils.findRenderedDOMComponentWithTag(result, 'textarea');
+      expect(textarea.getAttribute('id')).toBe('IM AN ID');
+    });
+
+    it('renders text before the text textarea', () => {
+      const label = TestUtils.findRenderedDOMComponentWithTag(result, 'label');
+      expect(label.children[0].textContent).toBe('IMA LABEL');
+    });
+
+  });
+});
