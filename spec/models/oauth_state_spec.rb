47c6cbe39215f85267553d70be07d1ad43217dc6
James Carbine <JamesCarbine@gmail.com>
2017-04-12 09:56:01 -0600

    Add several rspecs (#55)
    Single line validates state for OauthState

--- spec/models/oauth_state_spec.rb
+++ spec/models/oauth_state_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe OauthState, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "valid?" do
+    it "is true when state is unique" do
+      state = "asdf1234"
+      oauth_state = create(:oauth_state, state: state)
+      expect(oauth_state.valid?).to be true
+    end
+
+    it "is false for duplicate state" do
+      state = "asdf1234"
+      create(:oauth_state, state: state)
+      oauth_state = build(:oauth_state, state: state)
+      expect(oauth_state.valid?(state)).to be false
+    end
+
+    it "is false when state is missing" do
+      state = nil
+      oauth_state = build(:oauth_state, state: state)
+      expect(oauth_state.valid?).to be false
+    end
+  end
 end
