04065994534017b91c64bebdc1413f0172902d87
Justin Ball <justinball@gmail.com>
2017-04-19 20:06:09 -0600

    Fix setting name

--- client/libs/canvas/components/canvas_authentication.spec.jsx
+++ client/libs/canvas/components/canvas_authentication.spec.jsx
@@ -7,7 +7,7 @@ import Stub from '../../../specs_support/stub';
 describe('Canvas authentication', () => {
   it('renders a button to authenticate with Canvas', () => {
     const settings = {
-      canvas_oauth_path: 'http://www.example.com'
+      canvas_oauth_url: 'http://www.example.com'
     };
     const result = TestUtils.renderIntoDocument(
       <Stub><CanvasAuthentication settings={settings} /></Stub>
