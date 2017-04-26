883d429df2da7e98d87b74180f9363de14701b19
Justin Ball <justinball@gmail.com>
2017-04-26 17:20:04 -0600

    Fixes test

--- client/libs/canvas/components/content_item_selection_form.spec.jsx
+++ client/libs/canvas/components/content_item_selection_form.spec.jsx
@@ -6,14 +6,17 @@ import Stub from '../../../specs_support/stub';
 
 describe('Content Item Selection Form', () => {
   it('renders and submits a form to submit a content item response', () => {
-    const settings = {
-      canvas_oauth_url: 'http://www.example.com'
-    };
+    const launchData = {};
+    const contentItemReturnURL = 'http://www.example.com';
     const result = TestUtils.renderIntoDocument(
-      <Stub><ContentItemSelectionForm settings={settings} /></Stub>
+      <Stub>
+        <ContentItemSelectionForm
+          launchData={launchData}
+          contentItemReturnURL={contentItemReturnURL}
+        />
+      </Stub>
     );
-    const inputs = TestUtils.scryRenderedDOMComponentsWithTag(result, 'input');
-    const foundAuthorize = _.find(inputs, input => input.value === 'Authorize');
-    expect(_.isUndefined(foundAuthorize)).toBe(false);
+    const form = TestUtils.scryRenderedDOMComponentsWithTag(result, 'form');
+    expect(_.isUndefined(form)).toBe(false);
   });
 });
