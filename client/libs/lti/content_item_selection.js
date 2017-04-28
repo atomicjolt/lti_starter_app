98fb6c0f0e3a4f7e1fb007bce4379edd2b9df247
Justin Ball <justinball@gmail.com>
2017-04-28 17:44:58 -0600

    Add additional content item examples

--- client/libs/lti/content_item_selection.js
+++ client/libs/lti/content_item_selection.js
@@ -3,27 +3,12 @@
 // to respond to a content item selection request.
 // See the specification linked to below for more details.
 //
-// Content Item specification
+// Content Item specification:
 // https://www.imsglobal.org/specs/lticiv1p0/specification
 //
-// Example of 'content_items' json that needs to be sent to the tool consumer
-// content_items: {
-//  "@context": "http://purl.imsglobal.org/ctx/lti/v1/ContentItem",
-//  "@graph": [
-//    {
-//      "@type": "LtiLinkItem",
-//      "@id": "http://example.com/messages/launch",
-//      "url": "http://example.com/messages/launch",
-//      "title": "test",
-//      "text": "text",
-//      "mediaType": "application/vnd.ims.lti.v1.ltilink",
-//      "placementAdvice": {
-//        "presentationDocumentTarget": "frame"
-//      }
-//    }
-//  ]
-// }
-
+// Example content item responses can be found here:
+// https://www.imsglobal.org/specs/lticiv1p0/specification-3
+//
 
 // Wraps an array of graph items in the ContentItem hash
 export function contentItems(graph) {
@@ -35,7 +20,7 @@ export function contentItems(graph) {
 
 // Embeds html
 export function embedHtml(html) {
-  return [
+  return contentItems([
     {
       '@type': 'ContentItem',
       mediaType: 'text/html',
@@ -44,12 +29,72 @@ export function embedHtml(html) {
         presentationDocumentTarget: 'embed',
       },
     },
-  ];
+  ]);
+}
+
+// Embed multiple html items
+export function embedMultipleHtml(html1, html2) {
+  return contentItems([
+    {
+      '@type': 'ContentItem',
+      mediaType: 'text/html',
+      text: html1,
+      placementAdvice: {
+        presentationDocumentTarget: 'embed',
+      },
+    },
+    {
+      '@type': 'ContentItem',
+      mediaType: 'text/html',
+      text: html2,
+      placementAdvice: {
+        presentationDocumentTarget: 'embed',
+      },
+    },
+  ]);
+}
+
+// Embeds an png image
+export function embedImage(text, title, width, height, url) {
+  return contentItems([
+    { '@type': 'FileItem',
+      url,
+      mediaType: 'image/png',
+      text,
+      title,
+      placementAdvice: {
+        displayWidth: width,
+        displayHeight: height,
+        presentationDocumentTarget: 'embed'
+      }
+    }
+  ]);
+}
+
+// Embeds a thumbnail link as html
+export function imageLink(title, width, height, url) {
+  return contentItems([
+    {
+      '@type': 'ContentItem',
+      url,
+      mediaType: 'text/html',
+      thumbnail: {
+        '@id': 'http://developers.imsglobal.org/images/imscertifiedsm.png',
+        width,
+        height
+      },
+      title,
+      placementAdvice: {
+        presentationDocumentTarget: 'window',
+        windowTarget: '_blank'
+      }
+    }
+  ]);
 }
 
 // Adds an lti launch link
 export function ltiLaunch(name, launchURL) {
-  return [
+  return contentItems([
     {
       '@type': 'LtiLinkItem',
       mediaType: 'application/vnd.ims.lti.v1.ltilink',
@@ -70,17 +115,17 @@ export function ltiLaunch(name, launchURL) {
         scoreConstraints: {
           '@type': 'NumericLimits',
           normalMaximum: 10,
-          totalMaximum: 10,
-        },
-      },
-    },
-  ];
+          totalMaximum: 10
+        }
+      }
+    }
+  ]);
 }
 
 // Adds an iframe to the page as html
 export function embedIframe(iframeURL) {
   const iframe = `<iframe style="width: 100%; height: 500px;" src="${iframeURL}"></iframe>`;
-  return [
+  return contentItems([
     {
       '@type': 'ContentItem',
       mediaType: 'text/html',
@@ -89,21 +134,60 @@ export function embedIframe(iframeURL) {
         presentationDocumentTarget: 'embed',
       },
     },
-  ];
+  ]);
 }
 
 // Adds an iframe with an LTI launch
-export function embedLtiIframe(iframeURL) {
-  return [
+export function embedLtiIframe(url) {
+  return contentItems([
     {
       '@type': 'LtiLinkItem',
       mediaType: 'application/vnd.ims.lti.v1.ltilink',
-      text: 'iframe launched as lti',
-      url: iframeURL,
+      url,
       placementAdvice: {
         presentationDocumentTarget: 'iframe',
       },
     },
-  ];
+  ]);
 }
 
+// Adds an iframe with an LTI launch and grade write back
+// LineItem specification:
+// https://www.imsglobal.org/lti/model/uml/purl.imsglobal.org/vocab/lis/v2/outcomes/index.html#LineItem
+export function embedLtiIframeWriteBack(label, url) {
+  return {
+    '@context': [
+      'http://purl.imsglobal.org/ctx/lti/v1/ContentItem',
+      {
+        lineItem: 'http://purl.imsglobal.org/ctx/lis/v2/LineItem',
+        res: 'http://purl.imsglobal.org/ctx/lis/v2p1/Result#'
+      }
+    ],
+    '@graph': [
+      {
+        '@type': 'LtiLinkItem',
+        mediaType: 'application/vnd.ims.lti.v1.ltilink',
+        title: label,
+        url,
+        lineItem: {
+          '@type': 'LineItem',
+          label,
+          reportingMethod: 'res:totalScore',
+          assignedActivity: {
+            '@id': 'http://toolprovider.example.com/assessment/66400',
+            activityId: 'a-9334df-33'
+          },
+          scoreConstraints: {
+            '@type': 'NumericLimits',
+            normalMaximum: 10,
+            extraCreditMaximum: 0,
+            totalMaximum: 10,
+          },
+        },
+        placementAdvice: {
+          presentationDocumentTarget: 'iframe',
+        },
+      },
+    ],
+  };
+}
