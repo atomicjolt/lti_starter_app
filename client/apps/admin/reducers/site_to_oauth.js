182704a982dcb8c95d392a885e40c611e5d454b7
Justin Ball <justinball@gmail.com>
2017-05-31 14:53:25 -0600

    Fix warnings. Adds site url validation. Don't require OAuth id and key for new site. (#73)

--- client/apps/admin/reducers/site_to_oauth.js
+++ client/apps/admin/reducers/site_to_oauth.js
@@ -7,7 +7,14 @@ export default (state = initialState, action) => {
 
     case Constants.UPDATE_SITE_DONE:
     case Constants.CREATE_SITE_DONE:
-      return action.payload.url;
+
+      // Only request OAuth dance if all values are present
+      if (action.payload.url &&
+        action.payload.oauth_key &&
+        action.payload.oauth_secret) {
+        return action.payload;
+      }
+      return state;
 
     default:
       return state;
