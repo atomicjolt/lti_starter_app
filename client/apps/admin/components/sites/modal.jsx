3ba7eac72b998f909df8ce04173d7eacde5c81c6
Justin Ball <justinball@gmail.com>
2017-05-31 12:30:44 -0600

    Fix warnings. Adds site url validation. Don't require OAuth id and key for new site.

--- client/apps/admin/components/sites/modal.jsx
+++ client/apps/admin/components/sites/modal.jsx
@@ -1,9 +1,9 @@
+import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ReactModal from 'react-modal';
 
-import CanvasAuthentication from '../../../../libs/canvas/components/canvas_authentication';
 import SiteForm from './form';
 import * as SiteActions from '../../actions/sites';
 import { canvasDevKeysUrl } from '../../libs/sites';
@@ -26,7 +26,6 @@ export class SiteModal extends React.Component {
       lti_key: PropTypes.string,
       canvas_callback_url: PropTypes.string,
     }).isRequired,
-    siteToOauth: PropTypes.string,
   };
 
   constructor(props) {
@@ -42,35 +41,22 @@ export class SiteModal extends React.Component {
     });
   }
 
-  getCanvasAuthForm() {
-    if (this.props.siteToOauth) {
-      return (
-        <CanvasAuthentication
-          autoSubmit
-          hideButton
-          overrides={{
-            canvas_url: this.props.siteToOauth,
-            admin_url: window.location.href,
-            oauth_consumer_key: this.props.settings.lti_key,
-          }}
-        />
-      );
-    }
-
-    return null;
-  }
-
   setupSite() {
     const {
       site,
     } = this.state;
     const isUpdate = !!(site && site.id);
 
+    if (this.hasUrlError()) {
+      return;
+    }
+
     if (isUpdate) {
       this.props.updateSite(site);
     } else {
       this.props.createSite(site);
     }
+    this.props.closeModal();
   }
 
   siteChange(e) {
@@ -82,10 +68,16 @@ export class SiteModal extends React.Component {
     });
   }
 
+  hasUrlError() {
+    const { site } = this.state;
+    return site &&
+      site.url &&
+      site.url.length > 8 && // Must be longer than just https://
+      !_.startsWith(site.url, 'https://');
+  }
+
   render() {
-    const {
-      site,
-    } = this.state;
+    const { site } = this.state;
     const isUpdate = !!(site && site.id);
     const verb = isUpdate ? 'Update' : 'New';
 
@@ -106,6 +98,11 @@ export class SiteModal extends React.Component {
       );
     }
 
+    let urlError = null;
+    if (this.hasUrlError()) {
+      urlError = 'Url must begin with "https://"';
+    }
+
     return (
       <ReactModal
         isOpen={this.props.isOpen}
@@ -114,9 +111,9 @@ export class SiteModal extends React.Component {
         overlayClassName="unused"
         className="c-modal c-modal--site is-open"
       >
-        {this.getCanvasAuthForm()}
         <h2 className="c-modal__title">{verb} Domain</h2>
         <h3 className="c-modal__subtext">{canvasDevInstructions}</h3>
+        <h3 className="c-modal__subtext">{urlError}</h3>
         <SiteForm
           isUpdate={isUpdate}
           onChange={e => this.siteChange(e)}
