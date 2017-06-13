de403f2dad8801532771789d22d535c22e3ebb8c
Justin Ball <justinball@gmail.com>
2017-06-12 20:15:13 -0600

    Removes extraneous reducer

--- client/apps/hello_world/reducers/index.js
+++ client/apps/hello_world/reducers/index.js
@@ -3,14 +3,12 @@ import settings from '../../../libs/reducers/settings';
 import jwt from '../../../libs/reducers/jwt';
 import application from './application';
 import contentItemSelection from './content_item_selection';
-import ltiLaunch from './lti_launch';
 
 const rootReducer = combineReducers({
   settings,
   jwt,
   application,
-  contentItemSelection,
-  ltiLaunch
+  contentItemSelection
 });
 
 export default rootReducer;
