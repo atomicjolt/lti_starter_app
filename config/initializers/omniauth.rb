0dca6326de6003dd21fa4d3690a8bd800cec2988
Justin Ball <justinball@gmail.com>
2017-06-15 23:12:42 -0600

    Channel all OAuth requests through a single domain (#78)
    * Channel all OAuth requests through a single domain
    
    * remove puts
    
    * Fix redirect url in admin ui
    
    * Removes un-needed prompts
    
    * Fix OAuth parameters passed to client
    
    * Remove puts
    
    * Clean up
    
    * Fix the query
    
    * use lti key as secret
    
    * fixed and added specs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ OmniAuth.config.before_request_phase do |env|
   OauthState.create!(state: state, payload: request.params.to_json)
   env["omniauth.strategy"].options[:authorize_params].state = state
 
-  # Bye default omniauth will store all params in the session. The code above
+  # By default omniauth will store all params in the session. The code above
   # stores the values in the database so we remove the values from the session
   # since the amount of data in the original params object will overflow the
   # allowed cookie size
