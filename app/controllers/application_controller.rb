db1f0080b46c8c313b9d99225e5dda935de0eebf
Nick Benoit <nick.benoit14@gmail.com>
2017-06-27 16:03:48 -0600

    render static file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,11 +41,7 @@ class ApplicationController < ActionController::Base
   def set_lti_launch_values
     @is_lti_launch = true
     @canvas_url = current_application_instance.site.url
-    @app_name = if current_application_instance.disabled_at
-                  "disabled"
-                else
-                  current_application_instance.application.client_application_name
-                end
+    @app_name = current_application_instance.application.client_application_name
   end
 
 end
