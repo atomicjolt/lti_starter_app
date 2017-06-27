dc3f61e5044ee7ea411a9b83225e60af5902aeee
Nick Benoit <nick.benoit14@gmail.com>
2017-06-27 09:34:49 -0600

    fix squirly quotes

--- config/puma.rb
+++ config/puma.rb
@@ -22,7 +22,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # processes).
 #
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }.to_i
-worker_timeout ENV.fetch(“WEB_TIMEOUT”) { 60 }.to_i
+worker_timeout ENV.fetch("WEB_TIMEOUT") { 60 }.to_i
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
