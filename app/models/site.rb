3f68726933829fa9f2fa1086db6176da929e20ba
James Carbine <jamescarbine@gmail.com>
2017-07-17 16:15:24 -0600

    Make sure site url is saved in the database as just the url with no path

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,11 +3,20 @@ class Site < ActiveRecord::Base
 
   validates :url, presence: true, uniqueness: true
 
+  before_save :fix_url
+
   has_secure_token :secret
 
   def subdomain
     host = URI.parse(url).hostname
     host ? host.split(".").first : nil
+  end
+
+  def fix_url
+    parsed = URI.parse(url)
+    host = parsed.host
+    scheme = parsed.scheme
+    self.url = host ? "#{scheme}://#{host}" : nil
   end
 
 end
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -3,21 +3,35 @@ require "rails_helper"
 RSpec.describe Site, type: :model do
   describe "valid?" do
     it "is true when url is unique" do
-      url = "asdf1234"
+      url = "https://example.com"
       site = create(:site, url: url)
       expect(site.valid?).to be true
     end
 
     it "is false for duplicate url" do
-      url = "asdf1234"
+      url = "https://example.com"
       create(:site, url: url)
       site = build(:site, url: url)
+      expect(site.valid?(url)).to be false
+    end
+
+    it "is false for duplicate url with trailing slash" do
+      url = "https://example.com"
+      create(:site, url: url)
+      url = "https://example.com/"
+      site = create(:site, url: url)
       expect(site.valid?(url)).to be false
     end
 
     it "is false when url is missing" do
       url = nil
       site = build(:site, url: url)
+      expect(site.valid?).to be false
+    end
+
+    it "is false when url is bad" do
+      url = "https/meh"
+      site = create(:site, url: url)
       expect(site.valid?).to be false
     end
   end
