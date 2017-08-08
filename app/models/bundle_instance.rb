09026df26d34bf7bbf9bf2d73fc837bb7fcc9694
Nick Benoit <nick.benoit14@gmail.com>
2017-08-08 09:51:19 -0600

    remove helper

--- app/models/bundle_instance.rb
+++ app/models/bundle_instance.rb
@@ -6,10 +6,6 @@ class BundleInstance < ApplicationRecord
   has_secure_token :id_token
   before_save :fix_entity_key
 
-  def self.entity_key_from_url(url)
-    UrlHelper.host(url)
-  end
-
   def create_application_instances
     bundle&.applications&.map do |app|
       app.create_instance(site: site, bundle_instance: self)
@@ -17,6 +13,6 @@ class BundleInstance < ApplicationRecord
   end
 
   def fix_entity_key
-    self.entity_key = BundleInstance.entity_key_from_url(entity_key)
+    self.entity_key = UrlHelper.host(url)
   end
 end
