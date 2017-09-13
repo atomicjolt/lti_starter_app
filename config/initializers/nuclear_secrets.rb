55c4905b7ef8ae8c8626a806d7e0a31e954126be
Nick Benoit <nick.benoit14@gmail.com>
2017-09-13 07:49:04 -0600

    add nuclear secrets initializer

---
+++ config/initializers/nuclear_secrets.rb
@@ -0,0 +1,24 @@
+NuclearSecrets.configure do |config|
+  config.required_secrets = {
+    admin_name: String,
+    admin_email: String,
+    admin_password: String,
+    encryption_key: String,
+    application_name: String,
+    application_main_domain: String,
+    application_root_domain: String,
+    email_provider_username: String,
+    email_provider_password: String,
+    assets_url: String,
+    canvas_url: String,
+    canvas_developer_id: Fixnum,
+    canvas_developer_key: String,
+    auth0_client_id: String,
+    auth0_client_secret: String,
+    canvas_token: String,
+    hello_world_lti_secret: String,
+    admin_lti_secret: String,
+    secret_key_base: String,
+    secret_token: NilClass,
+  }
+end
