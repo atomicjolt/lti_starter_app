ef06dd1db8aa4fe1e7973e4bd153685513bb2225
Justin Ball <justinball@gmail.com>
2018-01-11 18:41:16 -0700

    Extracts out common spec functionality

--- spec/controllers/api/application_instances_controller_spec.rb
+++ spec/controllers/api/application_instances_controller_spec.rb
@@ -2,17 +2,8 @@ require "rails_helper"
 
 RSpec.describe Api::ApplicationInstancesController, type: :controller do
   before do
-    @user = FactoryGirl.create(:user)
-    @user.confirm
-    @user_token = AuthToken.issue_token({ user_id: @user.id })
-
-    @admin = FactoryGirl.create(:user)
-    @admin.confirm
-    @admin.add_to_role("administrator")
-    @admin_token = AuthToken.issue_token({ user_id: @admin.id })
-
-    @application_instance = FactoryGirl.create(:application_instance)
-    @application = @application_instance.application
+    setup_lti_users
+    setup_application_and_instance
   end
 
   context "no jwt" do
