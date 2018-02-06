831861f76b3dec789328020057e4a86cce2ffd11
Justin Ball <justinball@gmail.com>
2018-02-05 19:06:54 -0700

    Use local vars

--- app/controllers/api/ims_exports_controller.rb
+++ app/controllers/api/ims_exports_controller.rb
@@ -3,9 +3,9 @@ class Api::ImsExportsController < ApplicationController
   include Concerns::CanvasImsccSupport
 
   def show
-    @export = ImsExport.find_by(token: params[:id])
+    export = ImsExport.find_by(token: params[:id])
     respond_to do |format|
-      format.json { render json: @export.payload.merge(ims_export_id: @export.token) }
+      format.json { render json: export.payload.merge(ims_export_id: export.token) }
     end
   end
 
@@ -22,15 +22,15 @@ class Api::ImsExportsController < ApplicationController
       context_id: params[:context_id],
       lti_launch_tokens: lti_launches.pluck(:token),
     }
-    @export = ImsExport.create!(
+    export = ImsExport.create!(
       tool_consumer_instance_guid: params[:tool_consumer_instance_guid],
       context_id: params[:context_id],
       custom_canvas_course_id: params[:custom_canvas_course_id],
       payload: payload,
     )
     response = {
-      "status_url": status_api_ims_export_url(@export.token),
-      "fetch_url": api_ims_export_url(@export.token),
+      "status_url": status_api_ims_export_url(export.token),
+      "fetch_url": api_ims_export_url(export.token),
     }
     respond_to do |format|
       format.json { render json: response }
