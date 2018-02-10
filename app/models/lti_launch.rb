59230c2d0cdcf9c385008f1c9ddb624c534e02a0
Justin Ball <justinball@gmail.com>
2018-02-09 17:21:03 -0700

    Let the database handle the validation

--- app/models/lti_launch.rb
+++ app/models/lti_launch.rb
@@ -1,11 +1,4 @@
 class LtiLaunch < ApplicationRecord
   has_secure_token
   serialize :config, HashSerializer
-
-  validates :token,
-            uniqueness: {
-              scope: %i[
-                context_id
-              ],
-            }
 end
