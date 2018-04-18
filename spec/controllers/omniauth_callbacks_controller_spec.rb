3de3cde6acc19d2ff531d5660f45232ab3ec0d97
Justin Ball <justinball@gmail.com>
2018-04-17 19:09:52 -0600

    update spec

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -48,15 +48,15 @@ RSpec.describe OmniauthCallbacksController, type: :controller do
       request.env["omniauth.auth"] = get_canvas_omniauth(canvas_opts)
       user.lti_user_id = User.oauth_lti_user_id(request.env["omniauth.auth"])
       user.save!
-      oauth_complete_url = "http://example.com"
       response = get :canvas, params: {
-        oauth_complete_url: oauth_complete_url,
         canvas_url: "https://example.instructure.com",
+        oauth_consumer_key: "ltikey",
       }
 
-      expect(response).to redirect_to oauth_complete_url
       expect(warden.authenticated?(:user)).to be true
       expect(warden.user).to eq(user)
+      expect(response).to have_http_status 200
+      expect(assigns(:is_lti_launch)).to eq true
     end
 
     it "should redirect to origin without auth" do
