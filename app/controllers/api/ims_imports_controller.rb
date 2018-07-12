8a8233176b2ba2b48a60bf1b5e2d37597f7b3768
James Carbine <jamescarbine@gmail.com>
2018-07-12 11:04:53 -0600

    Ensure lti_launches exist Before processing lti_launches in an ims_import, ensure they actually exist first.

--- app/controllers/api/ims_imports_controller.rb
+++ app/controllers/api/ims_imports_controller.rb
@@ -2,15 +2,17 @@ class Api::ImsImportsController < ApplicationController
   include Concerns::CanvasImsccSupport
 
   def create
-    lti_launches = lti_launches_params(params[:data])[:lti_launches]
+    if params[:data] && params[:data][:lti_launches].present?
+      lti_launches = lti_launches_params(params[:data])[:lti_launches]
 
-    data = {
-      lti_launches: lti_launches,
-      context_id: params[:context_id],
-      tool_consumer_instance_guid: params[:tool_consumer_instance_guid],
-    }
+      data = {
+        lti_launches: lti_launches,
+        context_id: params[:context_id],
+        tool_consumer_instance_guid: params[:tool_consumer_instance_guid],
+      }
 
-    ImsImportJob.perform_later(data.to_json)
+      ImsImportJob.perform_later(data.to_json)
+    end
 
     render json: { status: "completed" }
   end
