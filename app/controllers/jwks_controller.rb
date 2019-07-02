ec99fa37275b98fee5ef78ad5bde43b71dc900b7
Justin Ball <justinball@gmail.com>
2019-07-02 12:29:42 -0600

    jwks from application instead of application instance

--- app/controllers/jwks_controller.rb
+++ app/controllers/jwks_controller.rb
@@ -2,7 +2,7 @@ class JwksController < ApplicationController
   def index
     respond_to do |format|
       # Map is required or the outer to_json will show your private keys to the world
-      format.json { render json: { keys: current_application_instance.jwks.map(&:to_json) }.to_json }
+      format.json { render json: { keys: current_application.jwks.map(&:to_json) }.to_json }
     end
   end
 end
