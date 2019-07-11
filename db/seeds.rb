3bf85400a3e97b1ff957388712aa2c797e676e46
Justin Ball <justinball@gmail.com>
2019-07-10 23:32:02 -0600

    Update client id

--- db/seeds.rb
+++ db/seeds.rb
@@ -135,7 +135,7 @@ applications = [
       {
         # Canvas
         iss: "https://canvas.instructure.com",
-        client_id: "43460000000000187",
+        client_id: "43460000000000194",
         jwks_url: LtiAdvantage::Definitions::CANVAS_PUBLIC_LTI_KEYS_URL,
         token_url: LtiAdvantage::Definitions::CANVAS_AUTH_TOKEN_URL,
         oidc_url: LtiAdvantage::Definitions::CANVAS_OIDC_URL,
@@ -143,7 +143,7 @@ applications = [
       {
         # Canvas Beta
         iss: "https://canvas.beta.instructure.com",
-        client_id: "43460000000000187",
+        client_id: "43460000000000194",
         jwks_url: LtiAdvantage::Definitions::CANVAS_BETA_PUBLIC_LTI_KEYS_URL,
         token_url: LtiAdvantage::Definitions::CANVAS_BETA_AUTH_TOKEN_URL,
         oidc_url: LtiAdvantage::Definitions::CANVAS_BETA_OIDC_URL,
