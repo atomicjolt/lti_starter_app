1c7b27ae6cdfc3982fb4bf57a0d288c93f7f239b
Justin Ball <justinball@gmail.com>
2019-07-17 18:31:48 -0600

    Adds spec for init controller action

--- spec/controllers/lti_launches_controller_spec.rb
+++ spec/controllers/lti_launches_controller_spec.rb
@@ -45,4 +45,30 @@ RSpec.describe LtiLaunchesController, type: :controller do
       expect(response.body).to include("lti_launch_config")
     end
   end
+
+  describe "init" do
+    before do
+      @iss = "https://canvas.instructure.com"
+      @application_instance.application.lti_installs.create!(
+        iss: @iss,
+        client_id: "43460000000000194",
+        jwks_url: LtiAdvantage::Definitions::CANVAS_PUBLIC_LTI_KEYS_URL,
+        token_url: LtiAdvantage::Definitions::CANVAS_AUTH_TOKEN_URL,
+        oidc_url: LtiAdvantage::Definitions::CANVAS_OIDC_URL,
+      )
+      allow(controller).to receive(:current_application).and_return(@application)
+    end
+    it "get a url that will be redirected to as part of the Open Id Connect process" do
+      params = {
+        "iss" => @iss,
+        "login_hint" => "0340cde37624c04979a6c3fdd0afc2479f8405ad",
+        "target_link_uri" => "https://helloworld.atomicjolt.xyz/lti_launches",
+        "lti_message_hint" => "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2ZXJpZmllciI6IjI5M2NlNDI4MjM3ZTAyNTc5MDg3ZmVkOTU5M2EwYzFmMDVmYmNhOTZhNTU3YjQyYjNiOWJiZmIwNTE4MDM4NmQwYzA5NjdmM2FhZDdhYzczODc4Yjk1YjM4NWJmMDVhNDU4MTBmZGZmYzNiMThmZDA5ZWY5YjMyOTUzZjNiOWUwIiwiY2FudmFzX2RvbWFpbiI6ImF0b21pY2pvbHQuaW5zdHJ1Y3R1cmUuY29tIiwiY29udGV4dF90eXBlIjoiQ291cnNlIiwiY29udGV4dF9pZCI6NDM0NjAwMDAwMDAwMDMzMzQsImV4cCI6MTU2MzQwNzk5OX0.7y8ZqEz4zwGvaBUV8puXCIINxnCulRGRg58jExPAhTo",
+        "controller" => "lti_launches",
+        "action" => "init",
+      }
+      post :init, params: params
+      expect(response).to have_http_status(302)
+    end
+  end
 end
