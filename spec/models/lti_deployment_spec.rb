e9fd2fb0fe539ac23384fa750f906bdffed7706c
Justin Ball <justinball@gmail.com>
2019-07-22 16:42:19 -0600

    More specs

--- spec/models/lti_deployment_spec.rb
+++ spec/models/lti_deployment_spec.rb
@@ -1,5 +1,25 @@
 require "rails_helper"
 
 RSpec.describe LtiDeployment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @application_instance = FactoryBot.create(:application_instance)
+  end
+
+  it "validates application instance" do
+    lti_deployment = LtiDeployment.new(deployment_id: "test")
+    expect(lti_deployment.valid?).to be false
+  end
+
+  it "validates deployment id" do
+    lti_deployment = LtiDeployment.new(application_instance: @application_instance)
+    expect(lti_deployment.valid?).to be false
+  end
+
+  it "creates an lti deployment" do
+    lti_deployment = LtiDeployment.new(
+      application_instance: @application_instance,
+      deployment_id: "test",
+    )
+    expect(lti_deployment.valid?).to be true
+  end
 end
