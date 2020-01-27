e53bac5f11cfecfbe94a73f955c75cb69e048180
David Spencer <david.spencer@atomicjolt.com>
2020-01-27 12:00:48 -0600

    Replace needing site with an application instance

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -238,26 +238,18 @@ Devise.setup do |config|
       request.session["canvas_url"] ||
       "https://canvas.instructure.com"
 
-    site = Site.find_by(url: url)
-    next unless site
+    application_instance = ApplicationInstance.find_by(lti_key: request.params["app_lti_key"])
+    next unless application_instance
 
-    if request.params["app_lti_key"].present?
-      application_instance = ApplicationInstance.find_by(lti_key: request.params["app_lti_key"])
-      next unless application_instance
+    env["omniauth.strategy"].options[:client_id] = application_instance.get_oauth_key
+    env["omniauth.strategy"].options[:client_secret] = application_instance.get_oauth_secret
 
-      env["omniauth.strategy"].options[:client_id] = application_instance.get_oauth_key
-      env["omniauth.strategy"].options[:client_secret] = application_instance.get_oauth_secret
-
-      if application_instance.oauth_scope.present?
-        env["omniauth.strategy"].options[:scope] = application_instance.oauth_scope.join(',')
-      end
-    else
-      env["omniauth.strategy"].options[:client_id] = site.oauth_key
-      env["omniauth.strategy"].options[:client_secret] = site.oauth_secret
+    if application_instance.oauth_scope.present?
+      env["omniauth.strategy"].options[:scope] = application_instance.oauth_scope.join(',')
     end
 
     custom_canvas_api_domain = request.params["custom_canvas_api_domain"]
-    site_url = custom_canvas_api_domain.present? ? "https://#{custom_canvas_api_domain}" : site.url
+    site_url = custom_canvas_api_domain.present? ? "https://#{custom_canvas_api_domain}" : url
     env["omniauth.strategy"].options[:client_options].site = site_url
   end
 
