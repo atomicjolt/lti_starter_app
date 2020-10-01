e4817c7405026d0bdac8ca7e91cab87bdba8489d
James Carbine <jamescarbine@gmail.com>
2020-09-30 21:06:17 -0600

    Update default mailer url host
    Use admin subdomain so invitations have the correct url to setup your
    account with.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,9 @@ Rails.application.configure do
   # }
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { host: Rails.application.secrets.application_root_domain }
+  config.action_mailer.default_url_options = {
+    host: "#{Application::ADMIN}.#{Rails.application.secrets.application_root_domain}",
+  }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
