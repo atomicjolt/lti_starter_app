30c27e87dd70bba4fecbe4a19a683d09fb2acc83
James Carbine <jamescarbine@gmail.com>
2021-04-12 19:13:18 -0600

    Remove unused migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_002834) do
+ActiveRecord::Schema.define(version: 2021_02_10_195029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
