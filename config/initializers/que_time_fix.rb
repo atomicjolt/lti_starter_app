6a5c53b58dad001e02d04d8c02f9d482ca412f8c
James Carbine <jamescarbine@gmail.com>
2021-05-20 12:56:44 -0600

    Add comment

--- config/initializers/que_time_fix.rb
+++ config/initializers/que_time_fix.rb
@@ -1,4 +1,5 @@
 # Fixes `TypeError (no implicit conversion of Time into String)` until we can go to version 1 of que.
+# Taken from https://github.com/que-rb/que/issues/247#issuecomment-595258236
 Que::Adapters::Base::CAST_PROCS[1184] = lambda do |value|
   case value
   when Time then value
