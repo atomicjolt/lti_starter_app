50f4b1da5f8ee3e34fccc097f16ba1218cf072b6
Carter Figgins <Carter.Figgins@atomicjolt.com>
2021-07-09 12:50:22 -0600

    removed catalyst

--- client/apps/admin/components/application_instances/extra_fields.jsx
+++ client/apps/admin/components/application_instances/extra_fields.jsx
@@ -1,10 +1,8 @@
-import CatalystLink from './catalyst_link';
 
 export default function getExtraFields(appKey) {
   if (appKey === 'search' || appKey === 'navigator') {
     return [{
-      name: 'Catalyst Stats',
-      Component: CatalystLink,
+
     }];
   }
   return [];
