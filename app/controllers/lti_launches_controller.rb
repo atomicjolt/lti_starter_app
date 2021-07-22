3279f23a461ff0b0daabaec3d6b3cfd3f8042a2b
Sean Collings <seanrcollings@gmail.com>
2021-07-22 10:59:14 -0600

    - turned all accesses in debug_data into safe accesses with `&.` - added it as a before action to the controller - added overflow-y

--- app/controllers/lti_launches_controller.rb
+++ app/controllers/lti_launches_controller.rb
@@ -7,13 +7,13 @@ class LtiLaunchesController < ApplicationController
 
   skip_before_action :verify_authenticity_token
   before_action :do_lti, except: [:init, :launch]
+  before_action :debug_data, except: [:init, :launch]
 
   def index
     if current_application_instance.disabled_at
       render file: File.join(Rails.root, "public", "disabled.html")
     end
 
-    debug_data
     if @lti_token
       # LTI advantage example code
       @lti_advantage_examples = LtiAdvantage::Examples.new(@lti_token, current_application_instance)
@@ -54,11 +54,10 @@ class LtiLaunchesController < ApplicationController
   def debug_data
     @debug_data = {
       "Tenant Name" => Apartment::Tenant.current,
-      "App Name" => current_application.name,
-      "Client App" => current_application.client_application_name,
-      "LTI Key" => current_application_instance.lti_key,
-      "Domain" => current_application_instance.domain,
-      "Test" => "something long that will wrap onto the next line of stuff hi there :)",
+      "App Name" => current_application&.name,
+      "Client App" => current_application&.client_application_name,
+      "LTI Key" => current_application_instance&.lti_key,
+      "Domain" => current_application_instance&.domain,
     }
   end
 
