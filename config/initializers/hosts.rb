df36914bbeba5fd23d2724b369a612bdb8e82c3e
Ryker Blunck <rykerblunck@gmail.com>
2021-10-18 13:41:51 -0600

    disable hostname checking because rails 6 broke it

--- config/initializers/hosts.rb
+++ config/initializers/hosts.rb
@@ -1,3 +1,9 @@
 # Allow requests from subdomains like 'www.yourcustomdomain.com'
 # and 'blog.yourcustomdomain.com'.
 Rails.application.config.hosts << ".#{Rails.application.secrets.application_root_domain}"
+
+# Rails 6 no longer allows underscores in domain names, which we still use for
+# some clients. This disables the host checking, so the line above is
+# essentially unused until find a better way to handle this
+# https://github.com/rails/rails/issues/41545
+Rails.application.config.hosts.clear
