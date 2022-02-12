1668c457891c1c295492daa4b848eb673b08b538
bfcoder <jamescarbine@gmail.com>
2022-02-11 18:38:38 -0700

    Change drift to 240 seconds

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@
 #
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
-Devise.otp_allowed_drift = 60 # value in seconds
+Devise.otp_allowed_drift = 240 # value in seconds
 Devise.setup do |config|
   config.warden do |manager|
     manager.default_strategies(:scope => :user).unshift :two_factor_authenticatable
