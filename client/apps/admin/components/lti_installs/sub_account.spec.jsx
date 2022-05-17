864a03a01ebb397175f61bc87339df6cb388510f
lautom36 <thomasjlau36@gmail.com>
2022-05-17 16:37:32 -0600

    refactored from class to function and fixed tests

--- client/apps/admin/components/lti_installs/sub_account.spec.jsx
+++ client/apps/admin/components/lti_installs/sub_account.spec.jsx
@@ -7,31 +7,36 @@ describe('should render nested accounts and active', () => {
   let instance;
   let clicked;
 
+  const accounts = {
+    1: {
+      id: 1,
+      name: 'account_name',
+      parent_account_id: null
+    },
+    2: {
+      id: 1,
+      name: 'account_name1',
+      parent_account_id: 1
+    },
+    3: {
+      id: 2,
+      name: 'account_name2',
+      parent_account_id: 1
+    }
+  };
+
+  const account = { id: 1, name: 'account_name', parent_account_id: null };
+  const setAccountActive = () => { clicked = true; };
+  const currentAccount = { id: 1 };
+
   beforeEach(() => {
     clicked = false;
-    const props = {
-      accounts: {
-        1: {
-          id: 1,
-          name: 'account_name',
-          parent_account_id: null
-        },
-        2: {
-          id: 1,
-          name: 'account_name1',
-          parent_account_id: 1
-        },
-        3: {
-          id: 2,
-          name: 'account_name2',
-          parent_account_id: 1
-        }
-      },
-      account: { id: 1, name: 'account_name', parent_account_id: null },
-      setAccountActive: () => { clicked = true; },
-      currentAccount: { id: 1 }
-    };
-    result = TestRenderer.create(<SubAccount {...props} />);
+    result = TestRenderer.create(<SubAccount
+      accounts={accounts}
+      account={account}
+      setAccountActive={setAccountActive}
+      currentAccount={currentAccount}
+    />);
     instance = result.root;
   });
 
@@ -40,18 +45,17 @@ describe('should render nested accounts and active', () => {
   });
 
   it('return correct amount of buttons', () => {
-    result.setState({ open: true });
-    const buttons = instance.findByType('button');
+    const buttons = instance.findAllByType('button');
     expect(buttons.length).toBe(1);
   });
 
   it('return render the dropdown', () => {
     const button = instance.findByType('button');
-    expect(button.props().children).toContain('account_name');
+    expect(button.props.children).toContain('account_name');
   });
 
   it('should not return the dropdown', () => {
-    const dropdown = instance.findByType('.i-dropdown');
+    const dropdown = instance.findAllByProps({ className: 'i-dropdown' });
     expect(dropdown.length).toBe(1);
   });
 
