e06f89336ff9c8990207ed36e570293d0f7a49a7
lautom36 <thomasjlau36@gmail.com>
2022-05-19 17:20:20 -0600

    fixed sites/modal tests and refactored

--- client/apps/admin/components/sites/modal.jsx
+++ client/apps/admin/components/sites/modal.jsx
@@ -1,129 +1,118 @@
 import _ from 'lodash';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ReactModal from 'react-modal';
 
 import SiteForm from './form';
-import * as SiteActions from '../../actions/sites';
+import { updateSite, createSite } from '../../actions/sites';
 import { canvasDevKeysUrl } from '../../libs/sites';
 
-const select = state => ({
-  settings: state.settings,
-  siteToOauth: state.siteToOauth
-});
+export default function SiteModal(props) {
+  const {
+    closeModal,
+    isOpen,
+    site: siteProps,
+  } = props;
 
-export class SiteModal extends React.Component {
-  static propTypes = {
-    site: PropTypes.shape({
-      id: PropTypes.number,
-    }),
-    createSite: PropTypes.func.isRequired,
-    updateSite: PropTypes.func.isRequired,
-    isOpen: PropTypes.bool.isRequired,
-    closeModal: PropTypes.func.isRequired,
-    settings: PropTypes.shape({
-      lti_key: PropTypes.string,
-      canvas_callback_url: PropTypes.string,
-    }).isRequired,
-  };
+  const [site, setSite] = useState(siteProps || {});
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      site: props.site || {},
-    };
-  }
+  const settings = useSelector((state) => state.settings);
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      site: nextProps.site,
-    });
-  }
+  const dispatch = useDispatch();
 
-  setupSite() {
-    const {
-      site,
-    } = this.state;
+  useEffect(() => {
+    setSite(siteProps);
+  }, [site]);
+
+  const hasUrlError = () => (
+    site
+      && site.url
+      && site.url.length > 8 // Must be longer than just https://
+      && !_.startsWith(site.url, 'https://')
+  );
+
+  const setupSite = () => {
     const isUpdate = !!(site && site.id);
 
-    if (this.hasUrlError()) {
+    if (hasUrlError()) {
       return;
     }
 
     if (isUpdate) {
-      this.props.updateSite(site);
+      dispatch(updateSite(site));
     } else {
-      this.props.createSite(site);
+      dispatch(createSite(site));
     }
-    this.props.closeModal();
-  }
+    closeModal();
+  };
 
-  siteChange(e) {
-    this.setState({
-      site: {
-        ...this.state.site,
+  const siteChange = (e) => {
+    setSite(
+      {
+        ...site,
         [e.target.name]: e.target.value
-      }
-    });
-  }
+      });
+  };
 
-  hasUrlError() {
-    const { site } = this.state;
-    return site &&
-      site.url &&
-      site.url.length > 8 && // Must be longer than just https://
-      !_.startsWith(site.url, 'https://');
-  }
+  const isUpdate = !!(site && site.id);
+  const verb = isUpdate ? 'Update' : 'New';
 
-  render() {
-    const { site } = this.state;
-    const isUpdate = !!(site && site.id);
-    const verb = isUpdate ? 'Update' : 'New';
-
-    const callbackUrl = this.props.settings.canvas_callback_url;
-    let canvasDevInstructions = 'Add a canvas domain first.';
-    if (site && site.url) {
-      canvasDevInstructions = (
+  const callbackUrl = settings.canvas_callback_url;
+  let canvasDevInstructions = 'Add a canvas domain first.';
+  if (site && site.url) {
+    canvasDevInstructions = (
+      <div>
         <div>
-          <div>
-            Use the redirect URI: {callbackUrl}
-          </div>
-          <div>
-            <a href={canvasDevKeysUrl(site)} className="c-modal__subtext-link" target="_blank" rel="noopener noreferrer" >
-              Get Canvas Developer Keys Here
-            </a>
-          </div>
+          Use the redirect URI:
+          {' '}
+          {callbackUrl}
         </div>
-      );
-    }
-
-    let urlError = null;
-    if (this.hasUrlError()) {
-      urlError = 'Url must begin with "https://"';
-    }
-
-    return (
-      <ReactModal
-        isOpen={this.props.isOpen}
-        onRequestClose={() => this.props.closeModal()}
-        contentLabel="Modal"
-        overlayClassName="unused"
-        className="c-modal c-modal--site is-open"
-      >
-        <h2 className="c-modal__title">{verb} Domain</h2>
-        <h3 className="c-modal__subtext">{canvasDevInstructions}</h3>
-        <h3 className="c-modal__subtext">{urlError}</h3>
-        <SiteForm
-          isUpdate={isUpdate}
-          onChange={e => this.siteChange(e)}
-          setupSite={() => this.setupSite()}
-          closeModal={() => this.props.closeModal()}
-          {...this.state.site}
-        />
-      </ReactModal>
+        <div>
+          <a href={canvasDevKeysUrl(site)} className="c-modal__subtext-link" target="_blank" rel="noopener noreferrer">
+            Get Canvas Developer Keys Here
+          </a>
+        </div>
+      </div>
     );
   }
+
+  let urlError = null;
+  if (hasUrlError()) {
+    urlError = 'Url must begin with "https://"';
+  }
+
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={() => closeModal()}
+      contentLabel="Modal"
+      overlayClassName="unused"
+      className="c-modal c-modal--site is-open"
+    >
+      <h2 className="c-modal__title">
+        {verb}
+        {' '}
+        Domain
+      </h2>
+      <h3 className="c-modal__subtext">{canvasDevInstructions}</h3>
+      <h3 className="c-modal__subtext">{urlError}</h3>
+      <SiteForm
+        isUpdate={isUpdate}
+        onChange={(e) => siteChange(e)}
+        setupSite={() => setupSite()}
+        closeModal={() => closeModal()}
+        {...site}
+      />
+    </ReactModal>
+  );
+
 }
 
-export default connect(select, { ...SiteActions })(SiteModal);
+SiteModal.propTypes = {
+  site: PropTypes.shape({
+    id: PropTypes.number,
+  }),
+  isOpen: PropTypes.bool.isRequired,
+  closeModal: PropTypes.func.isRequired,
+};
--- client/apps/admin/components/sites/modal.spec.jsx
+++ client/apps/admin/components/sites/modal.spec.jsx
@@ -1,29 +1,48 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
-import { SiteModal } from './modal';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import SiteModal from './modal';
+
+const settings = {
+  lti_key: 'abc123',
+  canvas_callback_url: 'http://example.com',
+};
+
+const mockStore = configureStore([]);
+const store = mockStore({ settings });
 
 describe('site modal', () => {
   let result;
-  let props;
 
   const url = 'bfcoder.com';
+  const isOpen = true;
+  const closeModal = () => {};
+  const site = {
+    id: 1,
+    url,
+  };
+
+  // https://medium.com/@amanverma.dev/mocking-create-portal-to-utilize-react-test-renderer-in-writing-snapshot-uts-c49773c88acd
+  beforeAll(() => {
+    ReactDOM.createPortal = jest.fn((element, node) => element);
+  });
+
+  afterEach(() => {
+    ReactDOM.createPortal.mockClear();
+  });
 
   beforeEach(() => {
-    props = {
-      isOpen: true,
-      closeModal: () => {},
-      site: {
-        id: 1,
-        url
-      },
-      createSite: () => {},
-      updateSite: () => {},
-      settings: {
-        lti_key: '',
-        canvas_callback_url: '',
-      },
-    };
-    result = TestRenderer.create(<SiteModal {...props} />);
+    result = TestRenderer.create(
+      <Provider store={store}>
+        <SiteModal
+          isOpen={isOpen}
+          closeModal={closeModal}
+          site={site}
+        />
+      </Provider>
+    );
   });
 
   it('matches the snapshot', () => {
