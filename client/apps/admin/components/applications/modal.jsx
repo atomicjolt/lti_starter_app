0ba7e06c7035f8aacbc2d938bbc32a153a78ec99
lautom36 <thomasjlau36@gmail.com>
2022-05-20 17:15:48 -0600

    fixed tests and refactored modal

--- client/apps/admin/components/applications/modal.jsx
+++ client/apps/admin/components/applications/modal.jsx
@@ -1,77 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ReactModal from 'react-modal';
 import Form from './form';
 
-export default class Modal extends React.Component {
+export default function Modal(props) {
+  const {
+    application: applicationProp,
+    isOpen,
+    closeModal,
+    save,
+  } = props;
 
-  static propTypes = {
-    application: PropTypes.shape({
-      name: PropTypes.string,
-      description: PropTypes.string,
-      default_config: PropTypes.string,
-      canvas_api_permissions: PropTypes.object,
-      oauth_key: PropTypes.string,
-      oauth_secret: PropTypes.string,
-    }),
-    isOpen: PropTypes.bool.isRequired,
-    closeModal: PropTypes.func.isRequired,
-    save: PropTypes.func,
-  };
+  const [application, setApplication] = useState(applicationProp);
+  const [configParseError, setConfigParseError] = useState(null);
 
-  constructor(props) {
-    super();
-    this.state = {
-      application: { ...props.application },
-      configParseError: null,
-    };
-  }
-
-  applicationChange(e) {
-    let configParseError = null;
+  const applicationChange = (e) => {
+    let newConfigParseError = null;
     if (e.target.name === 'default_config') {
       try {
         JSON.parse(e.target.value || '{}');
       } catch (err) {
-        configParseError = err.toString();
+        newConfigParseError = err.toString();
       }
     }
-    this.setState({
-      application: {
-        ...this.state.application,
-        [e.target.name]: e.target.value,
-      },
-      configParseError,
+
+    setApplication({
+      ...application,
+      [e.target.name]: e.target.value,
     });
-  }
 
-  saveApplication() {
-    this.props.save(this.state.application);
-  }
+    setConfigParseError(newConfigParseError);
+  };
 
-  render() {
-    return (
-      <ReactModal
-        isOpen={this.props.isOpen}
-        onRequestClose={() => this.props.closeModal()}
-        save={this.saveApplication}
-        contentLabel="Modal"
-        overlayClassName="c-modal__background"
-        className="c-modal c-modal--settings is-open"
-      >
-        <h2 className="c-modal__title">{this.state.application.name} Settings</h2>
-        <Form
-          description={this.state.application.description}
-          oauthKey={this.state.application.oauth_key}
-          oauthSecret={this.state.application.oauth_secret}
-          canvasApiPermissions={this.props.application.canvas_api_permissions}
-          defaultConfig={this.state.application.default_config}
-          configParseError={this.state.configParseError}
-          onChange={(e) => { this.applicationChange(e); }}
-          closeModal={() => this.props.closeModal()}
-          save={() => this.saveApplication()}
-        />
-      </ReactModal>
-    );
-  }
+  const saveApplication = () => {
+    save(application);
+  };
+
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={() => closeModal()}
+      save={saveApplication}
+      contentLabel="Modal"
+      overlayClassName="c-modal__background"
+      className="c-modal c-modal--settings is-open"
+    >
+      <h2 className="c-modal__title">
+        {application.name}
+        {' '}
+        Settings
+      </h2>
+      <Form
+        description={application.description}
+        oauthKey={application.oauth_key}
+        oauthSecret={application.oauth_secret}
+        canvasApiPermissions={application.canvas_api_permissions}
+        defaultConfig={application.default_config}
+        configParseError={configParseError}
+        onChange={(e) => { applicationChange(e); }}
+        closeModal={() => closeModal()}
+        save={() => saveApplication()}
+      />
+    </ReactModal>
+  );
 }
+
+Modal.propTypes = {
+  application: PropTypes.shape({
+    name: PropTypes.string,
+    description: PropTypes.string,
+    default_config: PropTypes.string,
+    canvas_api_permissions: PropTypes.object,
+    oauth_key: PropTypes.string,
+    oauth_secret: PropTypes.string,
+  }),
+  isOpen: PropTypes.bool.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  save: PropTypes.func,
+};
--- client/apps/admin/components/applications/modal.spec.jsx
+++ client/apps/admin/components/applications/modal.spec.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
+import ReactTestUtils, { act } from 'react-dom/test-utils';
 import Modal from './modal';
 
 describe('applications modal', () => {
   let result;
-  let props;
   let saved;
+
+  const application = {
+    name: 'SPEC_NAME',
+    description: 'SPEC_STRING'
+  };
+  const isOpen = true;
+  const closeModal = () => {};
+  const save = () => { saved = true; };
+
+  // https://medium.com/@amanverma.dev/mocking-create-portal-to-utilize-react-test-renderer-in-writing-snapshot-uts-c49773c88acd
+  beforeAll(() => {
+    ReactDOM.createPortal = jest.fn((element, node) => element);
+  });
+
+  afterEach(() => {
+    ReactDOM.createPortal.mockClear();
+  });
 
   beforeEach(() => {
     saved = false;
-    props = {
-      application: {
-        name        : 'SPEC_NAME',
-        description : 'SPEC_STRING'
-      },
-      isOpen: true,
-      closeModal: () => {},
-      save: () => { saved = true; },
-    };
 
-    result = TestRenderer.create(<Modal {...props} />);
+    result = TestRenderer.create(
+      <Modal
+        application={application}
+        isOpen={isOpen}
+        closeModal={closeModal}
+        save={save}
+      />);
   });
 
   it('modal matches snapshot', () => {
@@ -28,7 +43,25 @@ describe('applications modal', () => {
 
   it('saves the application', () => {
     expect(saved).toBeFalsy();
-    result.root.saveApplication();
+
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    act(() => {
+      ReactDOM.render(
+        <Modal
+          application={application}
+          isOpen={isOpen}
+          closeModal={closeModal}
+          save={save}
+        />,
+        container
+      );
+    });
+    const buttons = document.getElementsByTagName('button');
+    expect(buttons.length).toEqual(2);
+    const button = buttons[0];
+    ReactTestUtils.Simulate.click(button);
+
     expect(saved).toBeTruthy();
   });
 });
