85f88957fdf546978ee3610a0225977472f9dcbf
lautom36 <thomasjlau36@gmail.com>
2022-05-24 15:13:05 -0600

    fixed tests for sidebar

--- client/apps/admin/components/lti_installs/sidebar.spec.jsx
+++ client/apps/admin/components/lti_installs/sidebar.spec.jsx
@@ -1,36 +1,51 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import Sidebar from './sidebar';
+
+const mockStore = configureStore([]);
+const store = mockStore({});
 
 describe('lti installs sidebar', () => {
   let result;
   let instance;
   const applicationName = 'applicationName';
+  const accounts = {
+    1: {
+      id: 1,
+      name: 'accountName',
+      sub_accounts: []
+    }
+  };
+  const application = {
+    name: applicationName
+  };
+  const applicationInstance = {
+    site: {
+      url: 'www.atomicjolt.com'
+    }
+  };
+  const saveApplicationInstance = () => {};
+  const canvasRequest = () => {};
+  const setAccountActive = () => {};
+  const sites = {};
 
   describe('should render sidebar', () => {
     beforeEach(() => {
-      const props = {
-        accounts: {
-          1: {
-            id: 1,
-            name: 'accountName',
-            sub_accounts: []
-          }
-        },
-        application: {
-          name: applicationName
-        },
-        applicationInstance: {
-          site: {
-            url: 'www.atomicjolt.com'
-          }
-        },
-        saveApplicationInstance: () => {},
-        canvasRequest: () => {},
-        setAccountActive: () => {},
-        sites: {},
-      };
-      result = TestRenderer.create(<Sidebar {...props} />);
+      result = TestRenderer.create(
+        <Provider store={store}>
+          <Sidebar
+            accounts={accounts}
+            application={application}
+            applicationInstance={applicationInstance}
+            saveApplicationInstance={saveApplicationInstance}
+            canvasRequest={canvasRequest}
+            setAccountActive={setAccountActive}
+            sites={sites}
+          />
+        </Provider>
+      );
       instance = result.root;
     });
 
@@ -47,12 +62,12 @@ describe('lti installs sidebar', () => {
     });
 
     it('return the title name', () => {
-      const title = instance.findByProps({className: 'c-tool__title'});
+      const title = instance.findByProps({ className: 'c-tool__title' });
       expect(title.props.children).toBe(applicationName);
     });
 
     it('return the site url', () => {
-      const title = instance.findByProps({className: 'c-tool__instance'});
+      const title = instance.findByProps({ className: 'c-tool__instance' });
       expect(title.props.children.props.href).toEqual('www.atomicjolt.com');
     });
 
